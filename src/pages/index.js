e9774c427ef5a7bd994933cb8e938398051290a4
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-27 21:36:03 -0400

    Updated page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Portfolio Template" />
+    <SEO title="Andres Aponte" />
     <Header></Header>
     <Work></Work>
     <About></About>
