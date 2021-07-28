6500ee3abf07229295a214d443d124d08f077972
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-27 20:18:55 -0400

    Updated contact me section

--- src/yourdata.js
+++ src/yourdata.js
@@ -174,19 +174,19 @@ export default {
   contactSubHeading: "Let's create your next experience together",
   social: [
     // Add Or Remove The Link Accordingly
-    { img: githubIcon, url: "https://github.com/chetanverma16" },
-    {
-      img: codepenIcon,
-      url: "https://www.codepen.com/",
-    },
-    {
-      img: dribbbleIcon,
-      url: "https://dribbble.com/chetanverma",
-    },
-    {
-      img: instagramIcon,
-      url: "https://www.instagram.com/",
-    },
+    { img: githubIcon, url: "https://github.com/andresaponte22" },
+    // {
+    //   img: codepenIcon,
+    //   url: "https://www.codepen.com/",
+    // },
+    // {
+    //   img: dribbbleIcon,
+    //   url: "https://dribbble.com/",
+    // },
+    // {
+    //   img: instagramIcon,
+    //   url: "https://www.instagram.com/",
+    // },
   ],
 
   // End Contact Section ---------------
